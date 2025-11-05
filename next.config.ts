c405d0156387b9bc2413f96e06534f90c027d6fb
Devdeep8 <devdeeppatidar8@gmail.com>
2025-11-05 10:13:53 +0530

    dp

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    domains: [
+      "prabisha.com",
+      "bidisharay.com",
+      "prishatheexplorer.com",
+      "pratyushkumar.co.uk",
+      "res.cloudinary.com",
+      "intranet.prabisha.com",
+      "harrowbusiness.com",
+      "placehold.co",
+      "lh3.googleusercontent.com",
+      "randomuser.me",
+      "media-cdn.prabisha.com",
+    ],
     remotePatterns: [
       {
         protocol: "http",
@@ -16,11 +29,18 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: '/api/:path*',
+        source: "/api/:path*",
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
         ],
       },
     ];
