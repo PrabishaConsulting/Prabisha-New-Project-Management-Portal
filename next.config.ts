2afae927c385dc4a3c8c5f3463c0994036c5861b
TheDeCOderP <developerprakash10@gmail.com>
2026-05-04 15:45:02 +0530

    next config unwanted things removal

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     // ✅ Remove deprecated `domains`
     remotePatterns: [
